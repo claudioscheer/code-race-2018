1a72ce9717ec03e15c17c4cb7bf45f6448d347f3
Claudio Scheer <claudioscheer@protonmail.com>
2018-08-18 04:53:20 -0300

    Relatório

--- App/src/screens/proprietario/RelatorioProprietarioScreen.js
+++ App/src/screens/proprietario/RelatorioProprietarioScreen.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {
     FlatList,
     View,
+    StyleSheet,
     Text,
 } from 'react-native';
+import Swiper from 'react-native-swiper';
 import Toast from '../../componentes/Toast';
 import {
     Divider,
@@ -28,11 +30,47 @@ class RelatorioProprietarioScreen extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>Relório</Text>
-            </View>
+            <Swiper style={styles.wrapper} showsButtons>
+                <View style={styles.slide1}>
+                    <Text style={styles.text}>Hello Swiper</Text>
+                </View>
+                <View style={styles.slide2}>
+                    <Text style={styles.text}>Beautiful</Text>
+                </View>
+                <View style={styles.slide3}>
+                    <Text style={styles.text}>And simple</Text>
+                </View>
+            </Swiper>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+    },
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB'
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#97CAE5'
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#92BBD9'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
+});
 
 export default RelatorioProprietarioScreen;
